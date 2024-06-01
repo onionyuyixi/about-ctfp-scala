41966e6546d9f7d37f87ceb0ccc30afc281e787d
予一兮 <qingluanxi@gmail.com>
2024-06-02 03:14:19 +0800

    Semigroup Monoid

--- src/main/scala/onion_scalaz/Monoid.scala
+++ src/main/scala/onion_scalaz/Monoid.scala
@@ -1,8 +1,75 @@
 package onion_scalaz
 
+import scalaz.{Equal, Maybe}
+
+// 类似Category[=>:[_,_]]
 trait Monoid[F] extends Semigroup[F] {
 
+  // 类似于Category 中的 id方法
   def zero: F
 
+  def isMZero(a: F)(implicit eq: Equal[F]): Boolean =
+    eq.equal(a, zero)
 
+  final def ifEmpty[B](a: F)(t: => B)(f: => B)(implicit eq: Equal[F]): B =
+    if (isMZero(a)) {
+      t
+    } else f
+
+
+  final def onNotEmpty[B](a: F)(v: => B)(implicit eq: Equal[F], mb: Monoid[B]): B =
+    if (isMZero(a)) {
+      mb.zero
+    } else v
+
+
+  final def onEmpty[A, B](a: F)(v: => B)(implicit eq: Equal[F], mb: Monoid[B]): B =
+    ifEmpty(a)(mb.zero)(v)
+
+
+  def unfoldlSum[S](seed: S)(f: S => Maybe[(S, F)]): F =
+    unfoldlSumOpt(seed)(f) getOrElse zero
+
+  def unfoldrSum[S](seed: S)(f: S => Maybe[(F, S)]): F =
+    unfoldrSumOpt(seed)(f) getOrElse zero
+
+  final def category: Category[λ[(a, b) => F]] = new Category[λ[(a, b) => F]] with SemigroupCompose {
+    override def id[A]: F = zero
+  }
+
+  trait MonoidLaw extends SemigroupLaw {
+    def leftIdentity(a: F)(implicit F: Equal[F]): Boolean = F.equal(a, append(zero, a))
+
+    def rightIdentity(a: F)(implicit F: Equal[F]): Boolean = F.equal(a, append(a, zero))
+  }
+
+}
+
+
+object Monoid {
+
+  @inline def apply[F](implicit mf: Monoid[F]): Monoid[F] = mf
+
+
+  def fromIso[F, G](D: F <=> G)(implicit M: Monoid[G]): Monoid[F] = new Monoid[F] {
+    override def zero: F = D.from(M.zero)
+
+    override def append(f1: F, f2: => F): F = D.from(M.append(D.to(f1), D.to(f2)))
+  }
+
+  def instance[A](f: (A, => A) => A, z: A): Monoid[A] = new Monoid[A] {
+    override def zero: A = z
+
+    override def append(f1: A, f2: => A): A = f.curried(f1)(f2)
+  }
+
+}
+
+
+trait IsomorphismMonoid[F, G] extends Monoid[F] with IsomorphismSemigroup[F, G]{
+  implicit def G: Monoid[G]
+  ////
+
+  def zero: F = iso.from(G.zero)
+  ////
 }
--- src/main/scala/onion_scalaz/Semigroup.scala
+++ src/main/scala/onion_scalaz/Semigroup.scala
@@ -1,14 +1,79 @@
 
 package onion_scalaz
 
+import scalaz.{Equal, Maybe}
+import scalaz.Maybe.Just
+
+import scala.annotation.tailrec
+
+// 类似于Compose[=>:[_,_]]
 trait Semigroup[F] {
 
+  // 类似Compose[=>:[_,_]] 的compose方法
   def append(f1: F, f2: => F): F
 
 
+  protected[this] trait SemigroupCompose extends Compose[λ[(a, b) => F]] {
+    override def compose[A, B, C](f: F, g: F): F = append(f, g)
+  }
+
+  final def compose: Compose[λ[(α, β) => F]] =
+    new SemigroupCompose {}
 
 
+  def unfoldlSumOpt[S](seed: S)(f: S => Maybe[(S, F)]): Maybe[F] =
+    defaultUnfoldlSumOpt(seed)(f)
 
+  @inline private def defaultUnfoldlSumOpt[S](seed: S)(f: S => Maybe[(S, F)]): Maybe[F] = {
+    @tailrec
+    def go(s: S, acc: F): F = f(s) match {
+      case Maybe.Just((s, f)) => go(s, append(f, acc))
+      case Maybe.Empty() => acc
+    }
+
+    f(seed).map {
+      case (s, f) => go(s, f)
+    }
+
+  }
+
+  def unfoldrSumOpt[S](seed: S)(f: S => Maybe[(F, S)]): Maybe[F] =
+    defaultUnfoldrSumOpt(seed)(f)
+
+  @inline private def defaultUnfoldrSumOpt[S](seed: S)(f: S => Maybe[(F, S)]): Maybe[F] = {
+    @tailrec def go(acc: F, s: S): F = f(s) match {
+      case Just((f, s)) => go(append(acc, f), s)
+      case _ => acc
+    }
+
+    f(seed) map { case (a, s) => go(a, s) }
+  }
+
+
+  trait SemigroupLaw {
+    def associative(f1: F, f2: F, f3: F)(implicit F: Equal[F]): Boolean =
+      F.equal(append(f1, append(f2, f3)), append(append(f1, f2), f3))
+
+    def unfoldlSumOptConsistency[S](s: S, f: S => Maybe[(S, F)])(implicit E: Equal[F]): Boolean = {
+      val g: ((Int, S)) => Maybe[((Int, S), F)] = {
+        case (i, s) =>
+          if (i > 0) f(s) map { case (s, f) => ((i - 1, s), f) }
+          else Maybe.empty
+      }
+      val limit = 4 // to prevent infinite unfolds
+      Equal[Maybe[F]].equal(unfoldlSumOpt((limit, s))(g), defaultUnfoldlSumOpt((limit, s))(g))
+    }
+
+    def unfoldrSumOptConsistency[S](s: S, f: S => Maybe[(F, S)])(implicit E: Equal[F]): Boolean = {
+      val g: ((Int, S)) => Maybe[(F, (Int, S))] = {
+        case (i, s) =>
+          if (i > 0) f(s) map { case (f, s) => (f, (i - 1, s)) }
+          else Maybe.empty
+      }
+      val limit = 4 // to prevent infinite unfolds
+      Equal[Maybe[F]].equal(unfoldrSumOpt((limit, s))(g), defaultUnfoldrSumOpt((limit, s))(g))
+    }
+  }
 
 }
 
