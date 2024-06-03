69668017313cb508c458e04e09e173624ef76bd6
予一兮 <qingluanxi@gmail.com>
2024-06-03 10:47:11 +0800

    补充Apply

--- src/main/scala/onion_scalaz/morphism/Apply.scala
+++ src/main/scala/onion_scalaz/morphism/Apply.scala
@@ -1,6 +1,7 @@
 package onion_scalaz.morphism
 
-import scalaz.{Equal}
+import onion_scalaz.<~>
+import scalaz.{Equal, Id}
 
 
 trait Apply[F[_]] extends Functor[F] {
@@ -15,11 +16,6 @@ trait Apply[F[_]] extends Functor[F] {
   // apF实际上是ap方法一种抽象
   // 容器总morphism A=>B 变成了容器中数据之间的morphism
   def apF[A, B](f: => F[A => B]): F[A] => F[B] = ap(_)(f)
-
-  def apply2[A, B, C](fa: => F[A], fb: => F[B])(f: (A, B) => C): F[C] = {
-    val func1: F[B => C] = map(fa)(f.curried)
-    ap(fb)(func1)
-  }
 
 
   def ap2[A, B, C](fa: => F[A], fb: => F[B])(f: F[(A, B) => C]): F[C] = {
@@ -41,15 +37,49 @@ trait Apply[F[_]] extends Functor[F] {
   }
 
 
-  def ap4[A,B,C,D,E](fa: => F[A], fb: => F[B], fc: => F[C], fd: => F[D])(f: F[(A,B,C,D) => E]): F[E] =
+  def ap4[A, B, C, D, E](fa: => F[A], fb: => F[B], fc: => F[C], fd: => F[D])(f: F[(A, B, C, D) => E]): F[E] =
     ap(fd)(ap(fc)(ap(fb)(ap(fa)(map(f)(_.curried)))))
 
   // ap方法有很多衍生方法apN[_,_,...,_] 跟ap2一样  只是嵌套的更多了些
+  // 同样apply方法有很多衍生方法applyN[_,_,...,_] 跟apply2一样  只是嵌套的更多了些
+
+  // lift2方法做出了正确理解示范  将函数 (A,B)=>C lift 成 (F[A],F[B])=>F[C]
+  def lift2[A, B, C](f: (A, B) => C): (F[A], F[B]) => F[C] =
+    apply2(_, _)(f)
+
+  def apply2[A, B, C](fa: => F[A], fb: => F[B])(f: (A, B) => C): F[C] = {
+    // 使用了Functor的map方法 就是原函数被lift到新范畴F中
+    val func1: F[B => C] = map(fa)(f.curried)
+    ap(fb)(func1)
+  }
+
+  def apply3[A, B, C, D](fa: => F[A], fb: => F[B], fc: => F[C])(f: (A, B, C) => D): F[D] =
+    ap(fc)(ap(fb)(map(fa)(f.curried)))
+
+  def apply4[A, B, C, D, E](fa: => F[A], fb: => F[B], fc: => F[C], fd: => F[D])(f: (A, B, C, D) => E): F[E] =
+    ap(fd)(ap(fc)(ap(fb)(map(fa)(f.curried))))
 
 
+  // tuple方法是apply的特殊形式
+  def tuple2[A, B](fa: => F[A], fb: => F[B]): F[(A, B)] =
+    apply2(fa, fb)((_, _))
 
+  def tuple3[A, B, C](fa: => F[A], fb: => F[B], fc: => F[C]): F[(A, B, C)] =
+    apply3(fa, fb, fc)((_, _, _))
 
+  def tuple4[A, B, C, D](fa: => F[A], fb: => F[B], fc: => F[C], fd: => F[D]): F[(A, B, C, D)] =
+    apply4(fa, fb, fc, fd)((_, _, _, _))
 
+  def lift3[A, B, C, D](f: (A, B, C) => D): (F[A], F[B], F[C]) => F[D] =
+    apply3(_, _, _)(f)
+
+  def lift4[A, B, C, D, E](f: (A, B, C, D) => E): (F[A], F[B], F[C], F[D]) => F[E] =
+    apply4(_, _, _, _)(f)
+
+  // 抛弃掉左边
+  def discardLeft[A, B](fa: => F[A], fb: => F[B]): F[B] = apply2(fa, fb)((_, b) => b)
+
+  def discardRight[A, B](fa: => F[A], fb: => F[B]): F[A] = apply2(fa, fb)((a, _) => a)
 
 
   def compose[G[_]](implicit G0: Apply[G]): Apply[λ[a => F[G[a]]]] = new ApplyComposition[F, G] {
@@ -111,4 +141,25 @@ object Apply {
 
   @inline def apply[F[_]](implicit F: Apply[F]): Apply[F] = F
 
+
+  def fromIso[F[_], G[_]](D: F <~> G)(implicit E: Apply[G]): Apply[F] = new IsomorphismApply[F, G] {
+    override implicit def G: Apply[G] = E
+
+    override def iso: F <~> G = D
+  }
+
+}
+
+
+trait IsomorphismApply[F[_], G[_]] extends IsomorphismFunctor[F, G] with Apply[F] {
+
+  implicit def G: Apply[G]
+
+  override def ap[A, B](fa: => F[A])(f: => F[A => B]): F[B] =
+    iso.from(G.ap(iso.to(fa))(iso.to(f)))
+
+  override def apply2[A, B, C](fa: => F[A], fb: => F[B])(f: (A, B) => C): F[C] = {
+    val func: ((A, B) => C) => G[C] = G.apply2(iso.to(fa), iso.to(fb))
+    iso.from(func(f))
+  }
 }
